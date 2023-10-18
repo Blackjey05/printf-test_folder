52da843393efcae733701c3b820b2b6d44ad3fc9
marky376 <mmutuamark@gmail.com>
2023-10-18 21:37:20 +0300

    A commit on the custom printf projects.

--- _printf.c
+++ _printf.c
@@ -1,6 +1,7 @@
 #include "main.h"
-#include<stdarg.h>
-#include<string.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
 
 int _printf(const char *format, ...)
 {
--- main.h
+++ main.h
@@ -1,5 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int _printf(const char *format, ...);
 int _putchar(char c);
